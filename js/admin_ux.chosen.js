b6f03daf3aee06f662d805143a8feddc14fa4381
Oskar Schöldström <public@oxy.fi>
2015-08-17 13:47:13 -0500

    eslint: make js pass

--- js/admin_ux.chosen.js
+++ js/admin_ux.chosen.js
@@ -1,6 +1,8 @@
-(function($){
+(function ($) {
+  'use strict';
+
   Drupal.behaviors.admin_ux_chosen = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       if (jQuery.fn.chosen) {
         // Active chosen on additional settings, which are force disabled by
         // Chosen itself.
@@ -8,10 +10,10 @@
         // Force a width as calculations are wrong when element is hidden.
         // @see https://github.com/harvesthq/chosen/issues/92
         chosenSettings.width = '300px';
-        var field_ui = '[class*="form-item-additional-settings-"] select'
-          , context_ui = '.context-plugin-form select[multiple]';
+        var fieldUI = '[class*="form-item-additional-settings-"] select';
+        var contextUI = '.context-plugin-form select[multiple]';
 
-        $([field_ui, context_ui].join(', ')).chosen(chosenSettings);
+        $([fieldUI, contextUI].join(', ')).chosen(chosenSettings);
       }
     }
   };
--- js/admin_ux.metatag.js
+++ js/admin_ux.metatag.js
@@ -1,13 +1,15 @@
-(function($){
+(function ($) {
+  'use strict';
+
   /**
    * Display the current character count in the metatag description field.
    */
   Drupal.behaviors.admin_ux_metatag = {
-    attach: function(context, settings) {
-      var $metatags_description = $('#edit-metatags-description-value').once('admin-ux-metatag');
-      if ($metatags_description.length) {
-        $metatags_description.bind('keyup blur', Drupal.admin_ux.updateDescriptionCounter);
-        Drupal.admin_ux.updateDescriptionCounter.call($metatags_description[0]);
+    attach: function (context, settings) {
+      var $metatagsDescription = $('#edit-metatags-description-value').once('admin-ux-metatag');
+      if ($metatagsDescription.length) {
+        $metatagsDescription.bind('keyup blur', Drupal.admin_ux.updateDescriptionCounter);
+        Drupal.admin_ux.updateDescriptionCounter.call($metatagsDescription[0]);
       }
     }
   };
@@ -15,7 +17,7 @@
   Drupal.admin_ux = Drupal.admin_ux || {};
   Drupal.admin_ux.counter_selector = '#metatags-description-value-length .count';
 
-  Drupal.admin_ux.updateDescriptionCounter = function() {
+  Drupal.admin_ux.updateDescriptionCounter = function () {
     var length = this.value.length;
 
     $(Drupal.admin_ux.counter_selector).text(length > 150 ? 150 : length);
@@ -25,5 +27,4 @@
       return false;
     }
   };
-
 }(jQuery));
